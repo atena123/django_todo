6d11d3a624eb3316e1b6405e865d034fac9a962d
aneta <atena771@wp.pl>
2019-09-21 18:52:27 +0000

    Update ALLOWED_HOSTS

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '*8*5_ui#acw#_%e@%4lk596q9*ac^8gbl$2&d=@+18s!9q-@a4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54c7d6466e9e49718b03a8427cb372a3.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['54c7d6466e9e49718b03a8427cb372a3.vfs.cloud9.us-east-1.amazonaws.com',
+                    'simple-django-todo2.herokuapp.com']
 
 
 # Application definition
